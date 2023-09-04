c57b7efd0cb4d9bb7870bc3eb0be3dc5713f008f
hgkim1598 <120430851+hgkim1598@users.noreply.github.com>
2023-09-04 18:52:39 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -0,0 +1,11 @@
+def solution(number):
+    answer = 0
+    
+    for i in range(len(number)):
+        for j in range(i+1, len(number)):
+            for k in range(j+1, len(number)):
+                sum_ = 0
+                if sum_ + number[i] + number[j] + number[k] == 0:
+                    answer += 1
+                
+    return answer
